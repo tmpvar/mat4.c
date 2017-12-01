84554310a5d6456c3f84b8d0a97a9c5719eb3280
Tariel <sunzx@users.noreply.github.com>
2017-12-01 20:22:29 +0800

    Update create.h
    mat4 is a typedef of float *, so sizeof(mat4) is not 16 elements.

--- include/mat4/create.h
+++ include/mat4/create.h
@@ -10,7 +10,7 @@
  * @returns {mat4} a new 4x4 matrix
  */
 mat4 mat4_create() {
-    mat4 out = malloc(sizeof(mat4));
+    mat4 out = malloc(sizeof(float) * 16);
     out[0] = 1;
     out[1] = 0;
     out[2] = 0;
